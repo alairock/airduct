d4dea375ae891f563fa835a5351fff2849ae344c
Skyler Lewis <skyler.lewis@canopytax.com>
2019-09-03 11:59:23 -0600

    mark flow error, if a task that "cant fail" fails

--- airduct/database.py
+++ airduct/database.py
@@ -177,7 +177,7 @@ def mark_task(task, status="Complete", can_fail=False, message=''):
     })
     session.commit()
     if status is 'Error' and not can_fail:
-        mark_flow(task.flow_id)
+        mark_flow(task.flow_id, status=Error)
         return
     _update_children(task)
 
